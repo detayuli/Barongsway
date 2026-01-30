87370527842706902aeb0cd271b6cabd74cd91cf
VozSoldat <muhamaderril7@gmail.com>
2026-01-30 17:41:46 +0700

    Implement obstacles: fence and firecracker

---
+++ Assets/_Dev/Scripts/Core/Obstacles/Obstacle.cs
@@ -0,0 +1,18 @@
+using Barongslay.Core.Health;
+using UnityEngine;
+namespace Barongslay.Core.Obstacles
+{
+    [RequireComponent(typeof(BoxCollider2D))]
+    public class Obstacle : MonoBehaviour
+    {
+        [SerializeField] private int _damage = 1;
+        private void Start()
+        {
+            GetComponent<BoxCollider2D>().isTrigger = true;
+        }
+        void OnTriggerEnter2D(Collider2D collision)
+        {
+            collision.GetComponent<IDamagable>().TakeDamage(_damage);
+        }
+    }
+}
