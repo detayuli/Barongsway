269040049e62fe3cee610c69a70e638d6638968f
VozSoldat <muhamaderril7@gmail.com>
2026-01-31 18:20:25 +0700

    Update visibility

--- Assets/_Dev/Scripts/UI/PauseMenu.cs
+++ Assets/_Dev/Scripts/UI/PauseMenu.cs
@@ -6,40 +6,54 @@ public class PauseMenu : MonoBehaviour
 {
     public GameObject pauseButton;
     public GameObject pauseMenuUI;
+    bool gameIsPaused = false;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && !gameIsPaused)
+        {
+            PauseGame();
+            gameIsPaused = true;
+        }
+        if (Input.GetKeyDown(KeyCode.Escape) && gameIsPaused)
+        {
+            ResumeGame();
+            gameIsPaused = false;
+        }
+    }
 
     public void PauseGame()
     {
-        audiomanager.Instance.HandleButtonPress();
+        audiomanager.Instance?.HandleButtonPress();
         Time.timeScale = 0f;
-        //audiomanager.Instance.PlaySFX(audiomanager.Instance.buttonClick);
+        //audiomanager.Instance?.PlaySFX(audiomanager.Instance?.buttonClick);
         pauseButton.SetActive(false);
         pauseMenuUI.SetActive(true);
     }
     public void ResumeGame()
     {
-        audiomanager.Instance.HandleButtonPress();
+        audiomanager.Instance?.HandleButtonPress();
 
         Time.timeScale = 1f;
-        //audiomanager.Instance.PlaySFX(audiomanager.Instance.buttonClick);
+        //audiomanager.Instance?.PlaySFX(audiomanager.Instance?.buttonClick);
         pauseButton.SetActive(true);
         pauseMenuUI.SetActive(false);
     }
 
     public void RestartLevel()
     {
-        audiomanager.Instance.HandleButtonPress();
+        audiomanager.Instance?.HandleButtonPress();
 
         Time.timeScale = 1f;
-        //audiomanager.Instance.PlaySFX(audiomanager.Instance.buttonClick);
+        //audiomanager.Instance?.PlaySFX(audiomanager.Instance?.buttonClick);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
     public void QuitToMainMenu()
     {
-        audiomanager.Instance.HandleButtonPress();
+        audiomanager.Instance?.HandleButtonPress();
 
         Time.timeScale = 1f;
-        //audiomanager.Instance.PlaySFX(audiomanager.Instance.buttonClick);
+        //audiomanager.Instance?.PlaySFX(audiomanager.Instance?.buttonClick);
         SceneManager.LoadScene("MainMenu");
     }
 }
